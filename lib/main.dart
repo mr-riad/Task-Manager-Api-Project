f275cde2a2f0cbc12247a9a79d78f1dff7f287f9
mr-riad <riadgangni01302@gmail.com>
2025-07-05 03:18:35 +0600

    implement folder structure and fvm

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:task_manager_api/ui/screens/app.dart';
+
+void main(){
+ runApp(TaskManagerApp());
+}
---
+++ lib/ui/screens/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TaskManagerApp extends StatefulWidget {
+  const TaskManagerApp({super.key});
+
+  @override
+  State<TaskManagerApp> createState() => _TaskManagerAppState();
+}
+
+class _TaskManagerAppState extends State<TaskManagerApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
