754b99b6ba693091d620bc227db9f2171ecd4e62
mr-riad <riadgangni01302@gmail.com>
2025-07-07 23:41:02 +0600

    signin screen

--- lib/app.dart
+++ lib/app.dart
@@ -13,6 +13,39 @@ class _TaskManagerAppState extends State<TaskManagerApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 28
+          )
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor: Colors.white,
+          filled: true,
+          contentPadding: EdgeInsets.symmetric(horizontal: 16),
+          hintStyle: TextStyle(color: Colors.black45),
+          border: OutlineInputBorder(borderSide: BorderSide.none),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+          errorBorder: OutlineInputBorder(borderSide: BorderSide.none),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size.fromWidth(double.maxFinite),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 12),
+            backgroundColor: Colors.green,
+            foregroundColor: Colors.white,
+          ),
+        )
+      ),
       home: SplashScreen(),
 
     );
--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_manager_api/ui/screens/signIn_Screen.dart';
 
 import '../../utils/asset_paths.dart';
+import '../../widgets/screen_background.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  static const String name = '/';
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -12,21 +16,24 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    _moveToNextScreen();
+  }
+
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(Duration(seconds: 3));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => SigninScreen()),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SvgPicture.asset(
-            AssetPaths.backgroundSvg,
-            height: double.maxFinite,
-            width: double.maxFinite,
-            fit: BoxFit.cover,
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: SvgPicture.asset(AssetPaths.logoSvg),
-          ),
-        ],
+      body: ScreenBackground(
+        child: Center(child: SvgPicture.asset(AssetPaths.logoSvg)),
       ),
     );
   }
